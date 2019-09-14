7f57e8c8177e464a16834c96dceb564a79ba2747
anbutchers <43306564+anbutchers@users.noreply.github.com>
2019-09-13 21:45:58 -0400

    Updates for python version 3
    Updates for python version 3

--- address/address_list.py
+++ address/address_list.py
@@ -1,7 +1,9 @@
 from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
 import sys
 import os
-from address import Address, AddressParser
+from .address import Address, AddressParser
 
 
 if __name__ == '__main__':
